f251112c3c1e221e3dcd899c6000da5b9849ee96
vitorregisr <vitorregisrr@gmail.com>
2019-04-24 20:25:32 -0300

    Add listar page, Styles fixe

--- js/forms.js
+++ js/forms.js
@@ -2,62 +2,64 @@
 $(function () {
     'use strict';
 
-    var toolbarOptions = [
-        ['bold', 'italic', 'underline', 'strike'], // custom dropdow
-        ['blockquote', 'code-block'],
+    if ($('#editor-rich').length ) {
+        var toolbarOptions = [
+            ['bold', 'italic', 'underline', 'strike'], // custom dropdow
+            ['blockquote', 'code-block'],
 
-        [{
-            'header': 1
-        }, {
-            'header': 2
-        }], // custom button values
-        [{
-            'list': 'ordered'
-        }, {
-            'list': 'bullet'
-        }],
-        [{
-            'script': 'sub'
-        }, {
-            'script': 'super'
-        }], // superscript/subscript
-        [{
-            'indent': '-1'
-        }, {
-            'indent': '+1'
-        }], // outdent/indent
-        [{
-            'direction': 'rtl'
-        }], // text direction
+            [{
+                'header': 1
+            }, {
+                'header': 2
+            }], // custom button values
+            [{
+                'list': 'ordered'
+            }, {
+                'list': 'bullet'
+            }],
+            [{
+                'script': 'sub'
+            }, {
+                'script': 'super'
+            }], // superscript/subscript
+            [{
+                'indent': '-1'
+            }, {
+                'indent': '+1'
+            }], // outdent/indent
+            [{
+                'direction': 'rtl'
+            }], // text direction
 
-        [{
-            'align': []
-        }],
+            [{
+                'align': []
+            }],
 
-        ['image', 'clean'] // remove formatting button
-    ];
+            ['image', 'clean'] // remove formatting button
+        ];
 
-    const editor = new Quill('#editor-rich', {
-        modules: {
-            toolbar: toolbarOptions
-        },
-        placeholder: 'Compose an epic...',
-        theme: 'snow'
-    });
+        const editor = new Quill('#editor-rich', {
+            modules: {
+                toolbar: toolbarOptions
+            },
+            placeholder: 'Compose an epic...',
+            theme: 'snow'
+        });
 
-    const deltastore = $('#editor-rich').data('deltastore');
+        const deltastore = $('#editor-rich').data('deltastore');
 
-    if ($(deltastore).val()) {
-        try {
-            editor.setContents(JSON.parse($(deltastore).val()));
-        } catch {
-            console.log('Não foi possível parsear o conteúdo existente da notícia.')
+        if ($(deltastore).val()) {
+            try {
+                editor.setContents(JSON.parse($(deltastore).val()));
+            } catch {
+                console.log('Não foi possível parsear o conteúdo existente da notícia.')
+            }
         }
-    }
 
-    editor.on('text-change', function (delta, oldDelta, source) {
-        $(deltastore).val(JSON.stringify(editor.getContents()));
-    });
+        editor.on('text-change', function (delta, oldDelta, source) {
+            $(deltastore).val(JSON.stringify(editor.getContents()));
+        });
+    }
 
 
     //// TAGS INPUT (KEYWORDS) ////
